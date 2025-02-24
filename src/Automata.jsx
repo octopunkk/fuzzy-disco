ab4f2605a796a80961a6a2db4142665604bd519a
octopunkk <anais@anaisderue.com>
2025-02-24 21:33:58 +0100

    improved automata

--- src/Automata.jsx
+++ src/Automata.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect, useRef } from "react";
+import Window from "./Window";
 
-const colors = ["red", "green", "blue"];
+const colors = ["#15F5BA", "#836FFF", "#211951"];
 const deathColor = [1, 2, 0];
 
-const randomize = () => {
+const randomArray = () => {
   return new Array(30).fill(0).map(() => {
     return new Array(40).fill(0).map(() => Math.floor(Math.random() * 3));
   });
 };
 
-const initVal = randomize();
+const initVal = randomArray();
 
 const update = (cells) => {
   const nextCells = new Array(30).fill(0).map(() => {
@@ -61,26 +62,72 @@ export function Automata(props) {
   // cell value can be 0, 1, 2
   const cells = useRef(initVal);
   const canvas = useRef(null);
+  const [paused, setPaused] = useState(true);
+  const [settings, setSettings] = useState({
+    opened: false,
+  });
 
   const paint = (cells) => {
     const ctx = canvas.current.getContext("2d");
     for (let i = 0; i < cells.length; i++) {
       for (let j = 0; j < cells[0].length; j++) {
         ctx.fillStyle = colors[cells[i][j]];
-        ctx.fillRect(i * 10, j * 10, 10, 10);
+        ctx.fillRect(
+          (j * canvas.current.width) / cells[0].length,
+          (i * canvas.current.height) / cells.length,
+          canvas.current.width / cells[0].length,
+          canvas.current.height / cells.length
+        );
       }
     }
   };
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      cells.current = update(cells.current);
-      paint(cells.current);
-    }, 100);
-    return () => {
-      clearInterval(intervalId);
-    };
+    if (!paused) {
+      const intervalId = setInterval(() => {
+        cells.current = update(cells.current);
+        paint(cells.current);
+      }, 100);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [paused]);
+
+  useEffect(() => {
+    paint(cells.current);
   }, []);
 
-  return <canvas ref={canvas} id="canvas"></canvas>;
+  return (
+    <>
+      <canvas ref={canvas} id="canvas"></canvas>
+      {paused && (
+        <span
+          className="material-symbols-outlined pause"
+          onClick={() => setPaused((p) => !p)}
+        >
+          play_arrow
+        </span>
+      )}
+      {!paused && (
+        <span
+          className="material-symbols-outlined pause"
+          onClick={() => setPaused((p) => !p)}
+        >
+          pause
+        </span>
+      )}
+      <span
+        className="material-symbols-outlined settings"
+        onClick={() => setSettings((s) => ({ ...s, opened: !s.opened }))}
+      >
+        settings
+      </span>
+      {settings.opened && (
+        <Window height="200px" width="200px" windowStartPosition={[420, -150]}>
+          <p>Settings</p>
+        </Window>
+      )}
+    </>
+  );
 }
--- src/Window.jsx
+++ src/Window.jsx
@@ -4,24 +4,29 @@ import "./App.css";
 import Icon from "./Icon";
 
 function Window(props) {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(props.icon ? false : true);
   // TODO : finish this
   const [reduce, setReduce] = useState(false);
-  const { windowProps, pos } = useDraggable();
+  const { windowProps, pos } = useDraggable(null, {
+    startPosition: props.windowStartPosition,
+  });
 
   return (
     <div>
-      <Icon
-        path={props.icon}
-        openWindow={() => setOpen(true)}
-        startPosition={props?.startPosition}
-      />
+      {props.icon && (
+        <Icon
+          path={props.icon}
+          openWindow={() => setOpen(true)}
+          startPosition={props?.startPosition}
+        />
+      )}
       <div
         className="window"
         style={{
           display: open ? "" : "none",
           transform: `translate(${pos[0]}px, ${pos[1]}px)`,
           height: props.height ? props.height : "400px",
+          width: props.width ? props.width : "400px",
         }}
       >
         <div className="windowBar" {...windowProps}>
